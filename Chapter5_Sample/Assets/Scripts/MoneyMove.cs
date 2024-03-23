cbf3a419b72538400c0370fece7b0ceab4604c81
geunsuyoon <eulsu8704@gmail.com>
2024-03-23 21:56:29 +0900

    feat: Add assets

---
+++ Chapter5_Sample/Assets/Scripts/MoneyMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyMove : MonoBehaviour
+{
+    public Vector2 point;
+    Text txt;
+
+	// Use this for initialization
+	void Start ()
+    {
+        txt = transform.GetComponentInChildren<Text>();
+        txt.text = "+ " + GameManager.gm.moneyIncreaseAmount.ToString("###,###"); 
+        
+        Destroy(this.gameObject, 3f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //위치 이동
+        transform.position = Vector2.MoveTowards(transform.position, point, Time.deltaTime * 10f);
+
+        //스프라이트 컬러 페이드 아웃
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a - 0.01f);
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(point, 0.2f);
+    }
+}
---
+++ Chapter5_Sample/Assets/Scripts/Work.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Work : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //StartCoroutine(Move());
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(EventSystem.current.IsPointerOverGameObject() == false)
+            {               
+                GetComponent<Animator>().SetTrigger("click");
+            }
+
+            
+        }
+            
+    }
+
+
+    
+}
